345e186de8378135c9d3b28450671d572a3ad810
NguyenDoan85 <ericdoan2008@gmail.com>
2022-07-11 11:35:54 -0700

    add search city name function

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,44 @@ var dailyWeatherApiStarts =
 var dailyUVIndexApiStarts = "https://api.openweathermap.org/data/2.5/uvi?";
 var forecastWeatherApiStarts =
   "https://api.openweathermap.org/data/2.5/onecall?";
-// select from html element
-var searchCityForm = $("#searchCityForm");
-var searchedCities = $("#searchedCityLi");
+
+// designate field from html
+let searchCityForm = $("#searchCityForm");
+let searchedCities = $("#searchedCity");
+
+// request info from OpenWeather API start here
+// search city by name function
+var getCityWeather = function (cityName) {
+    // request the OpenWeather api url
+    var apiUrl =
+      dailyWeatherApiStarts + cityName + "&" + personalAPIKey + "&" + unit;
+    // make a request to url
+    fetch(apiUrl).then(function (response) {
+      if (response.ok) {
+        return response.json().then(function (response) {
+          $("#cityName").html(response.name);
+          // display time line
+          var unixTime = response.dt;
+          var date = moment.unix(unixTime).format("MM/DD/YY");
+          $("#currentdate").html(date);
+          // display weather icon
+          var weatherIconUrl =
+            "http://openweathermap.org/img/wn/" +
+            response.weather[0].icon +
+            "@2x.png";
+          $("#weatherIconToday").attr("src", weatherIconUrl);
+          $("#tempToday").html(response.main.temp + " \u00B0F");
+          $("#humidityToday").html(response.main.humidity + " %");
+          $("#windSpeedToday").html(response.wind.speed + " MPH");
+          // return coordinate for getUVIndex to call
+          var lat = response.coord.lat;
+          var lon = response.coord.lon;
+          getUVIndex(lat, lon);
+          getForecast(lat, lon);
+        });
+      } else {
+        alert("Please provide a valid city name.");
+      }
+    });
+  };
+
